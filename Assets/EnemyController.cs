63723753f84ffc67a2cf41c656db31f5d52393b1
DESKTOP-RFPHR6T\danie <danielyang432@gmail.com>
2021-05-20 20:03:54 +1000

    Fixing movment scripts, still work in progress

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class EnemyController : MonoBehaviour
+//{
+    //public float LookRadius = 20f;
+
+    //Transform target;
+
+    //Transform spawnPoint;
+
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    	//target = PlayerManager.instance.player.transform;
+    	//spawnPoint = EnemyManger.Instance.spawnPoint.transform; 
+    //}
+
+    //void OnDrawGizmosSelected()
+    //{
+    	//Gizmos.color = Color.red;
+    	//Gizmos.DrawWireSphere(transfor.position. LookRadus);
+    //}
+//}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,46 +9,90 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -12f;
     public float jumpHeight = 3f;
     public int jumpCount = 0;
+    public float wallForce = 3f;
 
+    public Transform wallCheck;
+    public bool onWall;
+    public float wallDistance = 0.4f;
+    public LayerMask wallMask;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
 
+
+    protected Vector3 currentImpact;
+
+
     Vector3 velocity;
     bool isGrounded;
     bool wallRun;
-    
+
 
     // Update is called once per frame
     void Update()
     {
+    	Jumping();
+
+    	WallBounce();
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+       	onWall = Physics.CheckSphere(wallCheck.position, wallDistance, wallMask);
+
+        //reset velocity and jumpcount
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            velocity.x = 0f;
+            velocity.z = 0f;
+            jumpCount = 0;
         }
-		
-         if(isGrounded)
-         {
-         	jumpCount = 0;
-         }
-
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        if(Input.GetButtonDown("Jump"))
+
+        velocity.y += gravity * Time.deltaTime;
+
+        if (isGrounded) 
+        {
+        	controller.Move(move * speed * Time.deltaTime);
+        	controller.Move(velocity * Time.deltaTime);
+        }
+        else 
+        {
+        	controller.Move(move * (speed / 2) * Time.deltaTime);
+        	controller.Move(velocity * Time.deltaTime);
+        }
+		
+    }
+	
+	//Wall bouncing
+    void WallBounce() 
+    {
+    	if (onWall && isGrounded == false) 
+    	{
+    		if (Input.GetButtonDown("Jump"))
+    		{
+    			velocity.x = 4f;
+    			controller.Move (velocity * Time.deltaTime);
+    		}
+    	}
+    }
+
+    //Jumping
+    void Jumping()
+    {
+		if(Input.GetButtonDown("Jump"))
         {
             if(jumpCount == 0)
             {
-            	 velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            	 jumpCount = 1;
+            	velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            	jumpCount = 1;
             }
             else
             {
@@ -56,27 +100,17 @@ public class PlayerMovement : MonoBehaviour
             }
         }
         else if (jumpCount == 1)
-        {
-        	velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        	jumpCount = 2;
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-		controller.Move(move * speed * Time.deltaTime);
-        controller.Move(velocity * Time.deltaTime);
+        	{
+        		velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        		jumpCount = 2;
+        	}
     }
 
-    private void OnControllerColliderHit (ControllerColliderHit hit){
-        if(!controller.isGrounded && hit.normal.y < 0.1f)
-        {
-            if(Input.GetButtonDown("Jump"))
-            {
-                Debug.DrawRay(hit.point, hit.normal, Color.red, 1.25f);
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-                velocity.x = hit.normal * speed;
-                
-            }
-            
-        }
+    protected virtual void Move()
+    {
+    	Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))
+    	movementInput *= speed * Time.deltaTime;
+    	transform.Translate(new Vector3(movementInput.x, 0f, movementInput.y));
     }
+
 }
